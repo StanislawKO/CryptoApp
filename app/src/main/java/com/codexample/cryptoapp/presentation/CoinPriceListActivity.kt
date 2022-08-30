0eb4cf9b6f85fe72f6f0182874a9c0727f5f6e28
Stanislav Kozh <printks@yandex.ru>
2022-08-30 21:26:13 +0300

    Use fragment

--- app/src/main/java/com/codexample/cryptoapp/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/codexample/cryptoapp/presentation/CoinPriceListActivity.kt
@@ -3,6 +3,7 @@ package com.codexample.cryptoapp.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.codexample.cryptoapp.R
 import com.codexample.cryptoapp.databinding.ActivityCoinPriceListBinding
 import com.codexample.cryptoapp.domain.CoinInfo
 import com.codexample.cryptoapp.presentation.adapters.CoinInfoAdapter
@@ -21,12 +22,11 @@ class CoinPriceListActivity : AppCompatActivity() {
         val adapter = CoinInfoAdapter(this)
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinInfo) {
-                val intent = CoinDetailActivity.newIntent(
-                    this@CoinPriceListActivity,
-                    coinPriceInfo.fromSymbol
-                )
-                startActivity(intent)
-//                Log.d("ON_CLICK_TEST", coinPriceInfo.fromSymbol)
+                if (isOnePaneMode()) {
+                    launchDetailActivity(coinPriceInfo.fromSymbol)
+                } else {
+                    launchDetailFragment(coinPriceInfo.fromSymbol)
+                }
             }
 
         }
@@ -37,5 +37,25 @@ class CoinPriceListActivity : AppCompatActivity() {
             adapter.submitList(it)
 //            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
         }
+    }
+
+    private fun isOnePaneMode() = binding.fragmentContainer == null
+
+    private fun launchDetailActivity(fromSymbol: String) {
+        val intent = CoinDetailActivity.newIntent(
+            this@CoinPriceListActivity,
+            fromSymbol
+        )
+        startActivity(intent)
+//                Log.d("ON_CLICK_TEST", coinPriceInfo.fromSymbol)
+    }
+
+    private fun launchDetailFragment(fromSymbol: String) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+            .addToBackStack(null)
+            .commit()
     }
 }
